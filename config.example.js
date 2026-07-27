bcf057c370d49b28a5e5456477a9504ffc00f594
Bojan Tripic <Bojan.Tripic@eFuelSystems.com>
2026-07-27 11:00:24 +0200

    Initial commit for GitHub Pages

---
+++ config.example.js
@@ -0,0 +1,7 @@
+(function (root) {
+  root.APP_CONFIG = {
+    // Zamenite URL-om vaseg deployovanog Google Apps Script web app-a.
+    // Primer: "https://script.google.com/macros/s/AKfycbxxxxxxx/exec"
+    appsScriptWebAppUrl: "",
+  };
+})(typeof window !== "undefined" ? window : this);
