6686286aeab7b9e6d80cf295c3e22b9ef99e7f8f
Bojan Tripic <Bojan.Tripic@eFuelSystems.com>
2026-07-27 11:42:23 +0200

    Update site

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 (function (root) {
   root.APP_CONFIG = {
-    // Zamenite URL-om vaseg deployovanog Google Apps Script web app-a.
-    // Primer: "https://script.google.com/macros/s/AKfycbxxxxxxx/exec"
     appsScriptWebAppUrl: "1MCa4s3oYEqZwojL3ETKKTssb-qjYrz9RqOm5BsUHOljOwaW0FchS5RkI",
   };
 })(typeof window !== "undefined" ? window : this);
