e56d98bb6efcb152f7b96b699eb707feb966b293
Bojan Tripic <Bojan.Tripic@eFuelSystems.com>
2026-08-18 12:37:50 +0200

    Update site

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 (function (root) {
   root.APP_CONFIG = {
-    appsScriptWebAppUrl: "https://script.google.com/macros/s/AKfycbwPe0pi1Q2JjR2QDKTXrPtndEUEBF8NOQhqkDblMA7QHCF-WNirGPI6mOD1Vgz2R8RT/exec",
+    appsScriptWebAppUrl: "https://script.google.com/macros/s/AKfycby4JzCfIAVljZtncTp6-N-ZD0GugVkH7hW3N8G3fTS6jncW0nS5oWkPpStz_vW0UJh1/exec",
     googleClientId: "555032185416-ov9reehi1rliv9ltvufk0sssm3iimqqg.apps.googleusercontent.com",
   };
 })(typeof window !== "undefined" ? window : this);
